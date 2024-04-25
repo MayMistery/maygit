573a0c9f69dfd90a2e589cb911b2d05127bd5bed
maybemia <1754704389@qq.com>
2024-04-25 12:50:31 +0800

    release 2.2.1 feat: ignore inline comment in ini

--- cmd/config.go
+++ cmd/config.go
@@ -17,10 +17,21 @@ type Config struct {
 }
 
 func LoadConfig(filePath string) (Config, error) {
-	var cfg = new(Config)
-	err := ini.MapTo(cfg, filePath)
-	if err != nil {
-		fmt.Print(err)
+	// 加载INI文件时使用自定义的LoadOptions
+	opts := ini.LoadOptions{
+		IgnoreInlineComment: true, // 忽略行内注释符
 	}
-	return *cfg, nil
+	cfg, err := ini.LoadSources(opts, filePath)
+	if err != nil {
+		fmt.Println("Failed to load INI file:", err)
+		return Config{}, err
+	}
+
+	var config Config
+	if err := cfg.MapTo(&config); err != nil {
+		fmt.Println("Failed to map configuration:", err)
+		return Config{}, err
+	}
+
+	return config, nil
 }
